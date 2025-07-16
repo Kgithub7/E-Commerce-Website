5ec2bfdf7090f50a8cd524a304fd3726cb3a9c41
Kobi <knjoku@hawk.iit.edu>
2025-07-15 23:58:50 -0500

    Display prices on each book and fix sorting by price.

--- books.js
+++ books.js
@@ -3,14 +3,20 @@ function renderBooks(filter) {
   const books = getBooks();
 
   if (filter === "LOW_TO_HIGH")
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort(
+      (a, b) =>
+        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+    );
   else if (filter === "HIGH_TO_LOW")
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
-  else if (filter === "RATING")
-    books.sort((a, b) => b.rating - a.rating);
+    books.sort(
+      (a, b) =>
+        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
+    );
+  else if (filter === "RATING") books.sort((a, b) => b.rating - a.rating);
 
-   const booksHtml=books.map((book) => {
-    return `<div class="book">
+  const booksHtml = books
+    .map((book) => {
+      return `<div class="book">
   <figure class="book__img--wrapper">
     <img class="book__img" src="${book.url}" alt="" />
   </figure>
@@ -19,26 +25,34 @@ function renderBooks(filter) {
   ${ratingsHTML(book.rating)}
   </div>
   <div class="book__price">
-    <span>$${book.originalPrice.toFixed(2)}
+    ${priceHTML(book.originalPrice, book.salePrice)}
   </div>
 </div>`;
-  }).join("");
-  booksWrapper.innerHTML=booksHtml
+    })
+    .join("");
+  booksWrapper.innerHTML = booksHtml;
 }
 
 function filterBooks(event) {
-    renderBooks(event.target.value)
+  renderBooks(event.target.value);
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) return `$${originalPrice.toFixed(2)}</span>`;
+  return `<span class="book__price--normal">$${originalPrice.toFixed(
+    2
+  )}</span>$${salePrice.toFixed(2)}`;
 }
 
 function ratingsHTML(rating) {
-    let ratingHTML = "";
-    for (i = 0; i < Math.floor(rating); ++i) {
-      ratingHTML += `<i class="fa-solid fa-star"></i>\n`;
-    }
-    if (!Number.isInteger(rating))
-      ratingHTML += `<i class="fa-solid fa-star-half-alt"></i>\n`;
-  console.log(ratingHTML)
-  return ratingHTML
+  let ratingHTML = "";
+  for (i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += `<i class="fa-solid fa-star"></i>\n`;
+  }
+  if (!Number.isInteger(rating))
+    ratingHTML += `<i class="fa-solid fa-star-half-alt"></i>\n`;
+  console.log(ratingHTML);
+  return ratingHTML;
 }
 
 setTimeout(() => {
